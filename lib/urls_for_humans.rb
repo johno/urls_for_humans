9fcd1e400acffa34513d5077b79ae131881d5916
John Otander <johnotander@gmail.com>
2014-04-14 17:53:08 -0600

    Implement urls_for_humans.

--- lib/urls_for_humans.rb
+++ lib/urls_for_humans.rb
@@ -1,23 +1,30 @@
 require "urls_for_humans/version"
-require "urls_for_humans/base"
 
 module UrlsForHumans
   def self.extended(model_class)
     return if model_class.respond_to?(:urls_for_humans)
 
     model_class.class_eval do
+      class << self
+        attr_accessor :humanly_attrs
+      end
+
       extend Base
       include Model
     end
   end
 
-  module Model
-    def humanly_attrs
-      self.class.humanly_attrs
+  module Base
+    def urls_for_humans(*humanly_attrs)
+      @humanly_attrs = humanly_attrs
     end
+  end
 
+  module Model
     def to_param
-      "#{ id }-#{ humanly_attrs.map(&:parameterize).join('-') }"
+      self.class.humanly_attrs.dup.unshift(:id).map do |attrib|
+        send(attrib).to_s.parameterize
+      end.reject(&:blank?).join('-')
     end
   end
 end
--- lib/urls_for_humans/base.rb
+++
@@ -1,7 +0,0 @@
-module UrlsForHumans
-  module Base
-    def urls_for_humans(*humanly_attrs)
-      @humanly_attrs = humanly_attrs
-    end
-  end
-end
